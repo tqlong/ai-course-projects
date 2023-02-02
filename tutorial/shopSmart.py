bea6a1f6aabe3ceaaf5a7730130f30241a23c2aa
Long Tran <tqlong@gmail.com>
2023-02-02 20:43:23 +0700

    shopSmart done

--- tutorial/shopSmart.py
+++ tutorial/shopSmart.py
@@ -29,8 +29,10 @@ def shopSmart(orderList, fruitShops):
         orderList: List of (fruit, numPound) tuples
         fruitShops: List of FruitShops
     """
-    "*** YOUR CODE HERE ***"
-    return None
+    return min(
+        [(shop.getPriceOfOrder(orderList), shop) for shop in fruitShops],
+        key=lambda x: x[0]
+    )[1]
 
 
 if __name__ == '__main__':
