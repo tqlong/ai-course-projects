6604a8e1c1db564517982213d39ac6ced9526365
Long Tran <tqlong@gmail.com>
2023-02-02 20:36:31 +0700

    buyLotsOfFruit done

--- tutorial/buyLotsOfFruit.py
+++ tutorial/buyLotsOfFruit.py
@@ -35,7 +35,11 @@ def buyLotsOfFruit(orderList):
     Returns cost of order
     """
     totalCost = 0.0
-    "*** YOUR CODE HERE ***"
+    for fruit, numPounds in orderList:
+        if fruit not in fruitPrices:
+            return None
+        else:
+            totalCost += numPounds * fruitPrices[fruit]
     return totalCost
 
 
