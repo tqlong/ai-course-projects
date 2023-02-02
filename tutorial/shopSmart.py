6f75956ed1ff88aecdf203611e641ee322ff7ced
Long Tran <tqlong@gmail.com>
2023-02-03 05:53:00 +0700

    final try

--- tutorial/shopSmart.py
+++ tutorial/shopSmart.py
@@ -29,7 +29,6 @@ def shopSmart(orderList, fruitShops):
         orderList: List of (fruit, numPound) tuples
         fruitShops: List of FruitShops
     """
-    return fruitShops[0]
     return min(
         [(shop.getPriceOfOrder(orderList), shop) for shop in fruitShops],
         key=lambda x: x[0]
