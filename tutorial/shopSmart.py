962ea87d45176fc06e430abf9222cc04ee00f3d6
haimeo1201 <hvhai87@gmail.com>
2023-02-07 01:55:09 +0700

    20020049

--- tutorial/shopSmart.py
+++ tutorial/shopSmart.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -30,6 +30,13 @@ def shopSmart(orderList, fruitShops):
         fruitShops: List of FruitShops
     """
     "*** YOUR CODE HERE ***"
+    minPrice = -1  # set to -1 to indicate that it has not been set yet
+    for shop in fruitShops:
+        totalPrice = shop.getPriceOfOrder(orderList)
+        if totalPrice < minPrice or minPrice == -1:
+            minPrice = totalPrice
+            bestShop = shop
+    return bestShop
     return None
 
 
@@ -41,6 +48,8 @@ if __name__ == '__main__':
     dir2 = {'apples': 1.0, 'oranges': 5.0}
     shop2 = shop.FruitShop('shop2', dir2)
     shops = [shop1, shop2]
-    print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+    print("For orders ", orders, ", the best shop is",
+          shopSmart(orders, shops).getName())
     orders = [('apples', 3.0)]
-    print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+    print("For orders: ", orders, ", the best shop is",
+          shopSmart(orders, shops).getName())
