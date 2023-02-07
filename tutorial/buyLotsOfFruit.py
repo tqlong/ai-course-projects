0b14fc83bce27cd37981cc78e2cbfeac63bc522f
thanhttruc <100545202+thanhttruc@users.noreply.github.com>
2023-02-07 22:25:03 +0700

    Update buyLotsOfFruit.py

--- tutorial/buyLotsOfFruit.py
+++ tutorial/buyLotsOfFruit.py
@@ -36,6 +36,8 @@ def buyLotsOfFruit(orderList):
     """
     totalCost = 0.0
     "*** YOUR CODE HERE ***"
+    for fruit, pound in orderList:
+        totalCost = totalCost + fruitPrices[fruit] * pound
     return totalCost
 
 
