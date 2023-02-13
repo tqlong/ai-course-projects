36704ba6435d95691a416a79ee1caa32a9768d52
Kientrung1202 <trungtp1202@gmail.com>
2023-02-13 17:08:32 +0700

    20020489_Nguyễn Kiên Trung

--- search/search.py
+++ search/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -18,6 +18,7 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
+
 
 class SearchProblem:
     """
@@ -70,7 +71,8 @@ def tinyMazeSearch(problem):
     from game import Directions
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return [s, s, w, s, w, w, s, w]
+
 
 def depthFirstSearch(problem):
     """
@@ -87,17 +89,69 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
+
+    tree = util.Stack()
+    tree.push((problem.getStartState(), []))
+
+    visitedNode = []
+
+    while (not tree.isEmpty()):
+        (state, path) = tree.pop()
+        if (problem.isGoalState(state)):
+            return path
+
+        if (state not in visitedNode):
+            visitedNode.append(state)
+            successors = problem.getSuccessors(state)
+            for successor in successors:
+                tree.push((successor[0], path + [successor[1]]))
+    return []
     util.raiseNotDefined()
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+    tree = util.Queue()
+    tree.push((problem.getStartState(), []))
+
+    visitedNode = []
+
+    while (not tree.isEmpty()):
+        (state, path) = tree.pop()
+        if (problem.isGoalState(state)):
+            return path
+
+        if (state not in visitedNode):
+            visitedNode.append(state)
+            successors = problem.getSuccessors(state)
+            for successor in successors:
+                tree.push((successor[0], path + [successor[1]]))
+    return []
     util.raiseNotDefined()
+
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    tree = util.PriorityQueue()
+    tree.push((problem.getStartState(), [], 0), 0)
+
+    visitedNode = []
+    while (not tree.isEmpty()):
+        (state, path, cost) = tree.pop()
+        if (problem.isGoalState(state)):
+            return path
+        if (state not in visitedNode):
+            visitedNode.append(state)
+            successors = problem.getSuccessors(state)
+            for successor in successors:
+                tree.push(
+                    (successor[0], path + [successor[1]], cost + successor[2]), cost + successor[2])
+    return []
+
     util.raiseNotDefined()
+
 
 def nullHeuristic(state, problem=None):
     """
@@ -106,9 +160,26 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
+
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    tree = util.PriorityQueue()
+    tree.push((problem.getStartState(), [], 0), 0)
+
+    visitedNode = []
+    while (not tree.isEmpty()):
+        (state, path, cost) = tree.pop()
+        if (problem.isGoalState(state)):
+            return path
+        if (state not in visitedNode):
+            visitedNode.append(state)
+            successors = problem.getSuccessors(state)
+            for successor in successors:
+                tree.push(
+                    (successor[0], path + [successor[1]], cost + successor[2]), cost + successor[2] +
+                    heuristic(successor[0], problem))
+    return []
     util.raiseNotDefined()
 
 
